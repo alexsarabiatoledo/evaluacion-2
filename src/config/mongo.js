38cec32aee7870f41adafe601feb9999aa23b4e5
alexsarabia <a.sarabia02@ufromail.cl>
2023-05-31 05:56:03 -0400

    last change was innecesary, so I fix it with this commit

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -7,10 +7,16 @@ const { MONGO_URI } = environment;
  * Mongo DB connection
  * @returns {Promise<mongoose>}
  */
-export async function connectDB() {
+async function connectDB() {
 	mongoose.set("strictQuery", true);
 	return mongoose.connect(MONGO_URI).catch((err) => {
 		console.log(`Failed in connectDB funcion: ${err}`);
 		process.exit(-1);
 	});
 }
+
+async function mongoDisconnect() {
+	await disconnect();
+}
+
+export {connectDB, mongoDisconnect };
