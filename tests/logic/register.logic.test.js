a458ac1c0463e457b3d26c73f6ba2ed38f9d720c
Alejandro-vy <a.vera07@ufromail.cl>
2023-06-01 22:59:54 -0400

    add changes test register and user

--- tests/logic/register.logic.test.js
+++ tests/logic/register.logic.test.js
@@ -1,15 +1,58 @@
 import { connectDB, mongoDisconnect } from "../../src/config/mongo";
 import { expect, jest } from "@jest/globals";
 import { UserModel } from '../../src/models/user.model';
-import {register} from "../../src/logic/register.logic"
+import { register } from "../../src/logic/register.logic";
+import { HTTPError } from "../../src/helpers/error.helper";
+import mongoose from "mongoose";
+
+jest.mock("../../src/services/registro-civil.service.js");
+jest.mock("../../src/models/user.model.js");
+jest.mock("../../src/services/notification.service.js");
+jest.mock("../../src/helpers/jwt.helper.js");
 
 
-describe("Business Logic: Register unit test", ()=>{
+const { MONGO_URI } = environment;
+
+describe("Business Logic: Register unit test", () => {
+	beforeAll(async () => {
+		await connectDB(MONGO_URI);
+		
+	});
+
+	afterAll(async () => {
+		await mongoose.connection.close();
+	});
 
 	it("[ERROR] When user is already registered", async () => {
-        
-		//mockear la función getCriminalRecords para devolver un valor true que indique que no tiene antecedentes
-        //mockear la función UserModel.findOne para devolver un objeto de usuario que coincida con el email o rut proporcionado
-        //resultado esperado: la función register lanza un httpError con código 400 indicando que el usuario ya existe
+		const existingUser = {
+			email: "avera@gmail.com",
+			rut: "20.565.923-4",
+		};
+
+		UserModel.findOne.mockResolvedValue(existingUser);
+		const getCriminalRecordsMock = jest.spyOn(
+			require("../../src/services/registro-civil.service.js"),
+			"getCriminalRecords"
+		);
+
+		getCriminalRecordsMock.mockResolvedValue(true);
+
+		const user = {
+		    name: "Alejandro Vera",
+        	email: "avera@gmail.com",
+            password: "1234",
+            rut: "20.565.923-4",
+            verified: false,
+            blocked: false,
+            code: "1qaz2wsx",
+		};
+
+		try {
+			await register(user);
+		} catch (error) {
+			expect(error).toBeInstanceOf(HTTPError);
+			expect(error.code).toEqual(400);
+			expect(error.msg).toEqual("El usuario ya existe");
+		}
 	});
-})
+});
